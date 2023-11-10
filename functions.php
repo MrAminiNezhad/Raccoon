fea946731056a02969e4d2454a549357bb02c78a
Ilya <86963582+ILYAGVC@users.noreply.github.com>
2023-11-10 17:35:33 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+     error_reporting(E_ERROR);
+
 function url_encode_full ($value) {
-return implode("", array_map(function($i) { return sprintf("%%%X", ord($i)); }, str_split($value)));
+     return implode("", array_map(function($i) { return sprintf("%%%X", ord($i)); }, str_split($value)));
 }
